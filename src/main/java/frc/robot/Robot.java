18ecb7464811e41e44a9d26d1541d71c4c85df77
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2024-02-13 20:11:39 -0600

    Add AdvantageKit logging
    Added AdvantageKit for extra diagnostics and logging for the robot. This will help hook into the AdvantageScope tool for WPILib.
    
    Added a Git metadata information task for each Gradle build, such as the commit SHA, the date of the build, the branch, and if the build was dirty (has changes not committed). This requires the installation of the Git CLI and be available on the PATH.
    
    Fixed issue where formatting failed a build. This will now perform the formatting on all builds, including deploy jobs.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,12 +5,20 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.HardwareConstants;
 import java.io.File;
 import java.io.IOException;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import swervelib.parser.SwerveParser;
 
 /**
@@ -19,7 +27,7 @@ import swervelib.parser.SwerveParser;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
 
   private static Robot instance;
   private Command m_autonomousCommand;
@@ -42,6 +50,30 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // Set up logging
+    Logger.recordMetadata("ProjectName", "2024-Robot"); // Set a metadata value
+
+    if (isReal()) {
+      Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+      new PowerDistribution(
+          HardwareConstants.REV_PDH_ID, ModuleType.kRev); // Enables power distribution logging
+    } else {
+      setUseTiming(false); // Run as fast as possible
+      String logPath =
+          LogFileUtil
+              .findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+      Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+      Logger.addDataReceiver(
+          new WPILOGWriter(
+              LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+    }
+    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+
+    // Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the
+    // "Understanding Data Flow" page
+    Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may
+    // be added.
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
