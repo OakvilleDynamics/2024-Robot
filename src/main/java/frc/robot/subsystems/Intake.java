64e8b3a020aa93366691cf9e4ad4dbec6e3bc3f2
Reid Akers <159196208+OakvilleDynamicsProgrammer@users.noreply.github.com>
2024-02-18 17:13:34 -0800

    Update Speed for intake and conveyor

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,10 +4,9 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.IdleMode;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MechanismConstants;
 
