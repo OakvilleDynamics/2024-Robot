32329ad0d8b845955e0b637f1c70cd36fcd1af59
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2024-03-14 14:26:52 -0500

    Reduce flywheel speeds

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -93,7 +93,7 @@ public final class Constants {
     public static final boolean FLYWHEEL_MOTOR_1_INVERTED = true;
     public static final boolean FLYWHEEL_MOTOR_2_INVERTED = false;
     public static final double FLYWHEEL_MOTOR_FULL_SPEED = 1.0;
-    public static final double FLYWHEEL_MOTOR_REDUCED_SPEED = 0.25;
+    public static final double FLYWHEEL_MOTOR_REDUCED_SPEED = 0.20;
     public static final double FLYWHEEL_MOTOR_SPEED_SLOW = 0.15;
   }
 }
