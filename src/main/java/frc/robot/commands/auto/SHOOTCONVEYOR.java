ea8568438c931739696bbc121ffb89965ba3f22e
ShaneHopkins11 <97139604+ShaneHopkins11@users.noreply.github.com>
2024-03-21 19:08:24 -0500

    Merge branch 'new-autopaths' of https://github.com/OakvilleDynamics/2024-Robot into new-autopaths

--- src/main/java/frc/robot/commands/auto/SHOOTCONVEYOR.java
+++ src/main/java/frc/robot/commands/auto/SHOOTCONVEYOR.java
@@ -5,20 +5,32 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Conveyor;
 
-public class SHOOT extends Command {
+public class SHOOTCONVEYOR extends Command {
   /** Creates a new SHOOT. */
-  public SHOOT() {
+  private final Conveyor m_conveyor;
+
+  private boolean m_finished = false;
+
+  public SHOOTCONVEYOR(Conveyor conveyor) {
+    m_conveyor = conveyor;
+    addRequirements(m_conveyor);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_finished = false;
+    m_conveyor.intakeConveyor();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_finished = true;
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -27,6 +39,6 @@ public class SHOOT extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_finished;
   }
 }
--- src/main/java/frc/robot/commands/auto/SHOOTFLYS.java
+++ src/main/java/frc/robot/commands/auto/SHOOTFLYS.java
@@ -5,20 +5,32 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.FlyWheel;
 
 public class SHOOTFLYS extends Command {
   /** Creates a new SHOOTFLYS. */
-  public SHOOTFLYS() {
+  private final FlyWheel m_flywheel;
+
+  private boolean m_finished = false;
+
+  public SHOOTFLYS(FlyWheel flywheel) {
+    m_flywheel = flywheel;
+    addRequirements(m_flywheel);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_finished = false;
+    m_flywheel.FLYWHEEL_MOTOR_FULL_SPEED();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_finished = true;
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -27,6 +39,6 @@ public class SHOOTFLYS extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_finished;
   }
 }
