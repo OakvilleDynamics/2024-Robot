10b7014733de7e20650a744570a178268ad01a70
Jim Hopkins - Windows 10 Laptop <jim@jhopkins.us>
2024-06-09 13:05:47 -0500

    Revert to commit 5cb866b

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -65,7 +65,6 @@ public final class Constants {
     public static final double LEFT_X_DEADBAND = 0.1;
     public static final double LEFT_Y_DEADBAND = 0.1;
     public static final double RIGHT_X_DEADBAND = 0.1;
-    public static final double RIGHT_Y_DEADBAND = 0.1;
     public static final double TURN_CONSTANT = 6;
   }
 
@@ -126,7 +125,6 @@ public final class Constants {
       public static final double PITCH = Units.degreesToRadians(90.0); // degrees to radians
       public static final double YAW = Units.degreesToRadians(180.0); // degrees to radians
     }
-
     public static final int FLYWHEEL_MOTOR_SPEED_ONEHUNDRED = 1;
   }
 }
--- src/main/java/frc/robot/commands/swervedrive/drivebase/AbsoluteFieldDrive.java
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/AbsoluteFieldDrive.java
@@ -1,26 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands.swervedrive.drivebase;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
-import frc.robot.util.swerve.SwerveController;
-import frc.robot.util.swerve.SwerveMath;
 import java.util.List;
 import java.util.function.DoubleSupplier;
+import swervelib.SwerveController;
+import swervelib.math.SwerveMath;
 
-public class AbsoluteFieldDrive extends CommandBase {
+/** An example command that uses an example subsystem. */
+public class AbsoluteFieldDrive extends Command {
+
   private final SwerveSubsystem swerve;
-  private final DoubleSupplier vX;
-  private final DoubleSupplier vY;
-  private final DoubleSupplier heading;
+  private final DoubleSupplier vX, vY, heading;
 
   /**
-   * Absolute drive based on field orientation.
+   * Used to drive a swerve robot in full field-centric mode. vX and vY supply translation inputs,
+   * where x is torwards/away from alliance wall and y is left/right. headingHorzontal and
+   * headingVertical are the Cartesian coordinates from which the robot's angle will be derived-
+   * they will be converted to a polar angle, which the robot will rotate to.
    *
-   * @param swerve The swerve subsystem
+   * @param swerve The swerve drivebase subsystem.
    * @param vX DoubleSupplier that supplies the x-translation joystick input. Should be in the range
    *     -1 to 1 with deadband already accounted for. Positive X is away from the alliance wall.
    * @param vY DoubleSupplier that supplies the y-translation joystick input. Should be in the range
@@ -31,8 +39,8 @@ public class AbsoluteFieldDrive extends CommandBase {
   public AbsoluteFieldDrive(
       SwerveSubsystem swerve, DoubleSupplier vX, DoubleSupplier vY, DoubleSupplier heading) {
     this.swerve = swerve;
-    this.vX = () -> applyDeadband(vX.getAsDouble(), 0.05);  // Applying deadband
-    this.vY = () -> applyDeadband(vY.getAsDouble(), 0.05);  // Applying deadband
+    this.vX = vX;
+    this.vY = vY;
     this.heading = heading;
 
     addRequirements(swerve);
@@ -46,6 +54,7 @@ public class AbsoluteFieldDrive extends CommandBase {
   public void execute() {
 
     // Get the desired chassis speeds based on a 2 joystick module.
+
     ChassisSpeeds desiredSpeeds =
         swerve.getTargetSpeeds(
             vX.getAsDouble(), vY.getAsDouble(), new Rotation2d(heading.getAsDouble() * Math.PI));
@@ -76,14 +85,5 @@ public class AbsoluteFieldDrive extends CommandBase {
   @Override
   public boolean isFinished() {
     return false;
-  }
-
-  // Deadband utility function
-  public static double applyDeadband(double value, double deadband) {
-    if (Math.abs(value) > deadband) {
-      return (value - Math.signum(value) * deadband) / (1.0 - deadband);
-    } else {
-      return 0.0;
-    }
   }
 }
