b926c896bd0308c83c4de60bce43d1116d73ca86
Jim Hopkins - Windows 10 Laptop <jim@jhopkins.us>
2024-06-09 13:39:47 -0500

    updates for strafing issues - jh

--- src/main/java/frc/robot/commands/swervedrive/drivebase/AbsoluteFieldDrive.java
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/AbsoluteFieldDrive.java
@@ -1,89 +1,56 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.swervedrive.drivebase;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
-import java.util.List;
 import java.util.function.DoubleSupplier;
-import swervelib.SwerveController;
-import swervelib.math.SwerveMath;
 
-/** An example command that uses an example subsystem. */
-public class AbsoluteFieldDrive extends Command {
+// will need to update commandbase in the future, but attempting to see if this fixes the strafing issues currently being encountered
+public class AbsoluteFieldDrive extends CommandBase {
+  private static final double DEADBAND = 0.1;
+  private final SwerveSubsystem swerveSubsystem;
+  private final DoubleSupplier forward;
+  private final DoubleSupplier strafe;
+  private final DoubleSupplier rotation;
 
-  private final SwerveSubsystem swerve;
-  private final DoubleSupplier vX, vY, heading;
-
-  /**
-   * Used to drive a swerve robot in full field-centric mode. vX and vY supply translation inputs,
-   * where x is torwards/away from alliance wall and y is left/right. headingHorzontal and
-   * headingVertical are the Cartesian coordinates from which the robot's angle will be derived-
-   * they will be converted to a polar angle, which the robot will rotate to.
-   *
-   * @param swerve The swerve drivebase subsystem.
-   * @param vX DoubleSupplier that supplies the x-translation joystick input. Should be in the range
-   *     -1 to 1 with deadband already accounted for. Positive X is away from the alliance wall.
-   * @param vY DoubleSupplier that supplies the y-translation joystick input. Should be in the range
-   *     -1 to 1 with deadband already accounted for. Positive Y is towards the left wall when
-   *     looking through the driver station glass.
-   * @param heading DoubleSupplier that supplies the robot's heading angle.
-   */
   public AbsoluteFieldDrive(
-      SwerveSubsystem swerve, DoubleSupplier vX, DoubleSupplier vY, DoubleSupplier heading) {
-    this.swerve = swerve;
-    this.vX = vX;
-    this.vY = vY;
-    this.heading = heading;
+      SwerveSubsystem swerveSubsystem,
+      DoubleSupplier forward,
+      DoubleSupplier strafe,
+      DoubleSupplier rotation) {
+    this.swerveSubsystem = swerveSubsystem;
+    this.forward = forward;
+    this.strafe = strafe;
+    this.rotation = rotation;
+    addRequirements(swerveSubsystem);
+  }
 
-    addRequirements(swerve);
+  private double applyDeadband(double value) {
+    return Math.abs(value) > DEADBAND ? value : 0.0;
   }
 
   @Override
-  public void initialize() {}
+  public void initialize() {
+    // Initialization code if needed
+  }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
-    // Get the desired chassis speeds based on a 2 joystick module.
-
-    ChassisSpeeds desiredSpeeds =
-        swerve.getTargetSpeeds(
-            vX.getAsDouble(), vY.getAsDouble(), new Rotation2d(heading.getAsDouble() * Math.PI));
-
-    // Limit velocity to prevent tippy
-    Translation2d translation = SwerveController.getTranslation2d(desiredSpeeds);
-    translation =
-        SwerveMath.limitVelocity(
-            translation,
-            swerve.getFieldVelocity(),
-            swerve.getPose(),
-            Constants.LOOP_TIME,
-            Constants.ROBOT_MASS,
-            List.of(Constants.CHASSIS),
-            swerve.getSwerveDriveConfiguration());
-    SmartDashboard.putNumber("LimitedTranslation", translation.getX());
-    SmartDashboard.putString("Translation", translation.toString());
-
-    // Make the robot move
-    swerve.drive(translation, desiredSpeeds.omegaRadiansPerSecond, true);
+    swerveSubsystem.drive(
+        new Translation2d(
+            applyDeadband(forward.getAsDouble()), applyDeadband(strafe.getAsDouble())),
+        applyDeadband(rotation.getAsDouble()),
+        true // Assuming the drive method requires a boolean for field-relative
+        );
   }
 
-  // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    swerveSubsystem.drive(new Translation2d(0, 0), 0, true); // Stop the robot when the command ends
+  }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return false; // Ensure the command never finishes on its own
   }
 }
